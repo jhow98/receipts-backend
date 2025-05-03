3e01e2e3604ee2737e486a805ceea55e4c169269
Jhonatan <jhonatanlopes98@outlook.com>
2025-05-03 02:47:01 -0300

    fix(recipes): cors authorization added

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,9 @@ async function bootstrap() {
 
   app.enableCors({
     origin: '*',
-    credentials: true,
+    methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Authorization'],
   });
 
   app.enableShutdownHooks();
